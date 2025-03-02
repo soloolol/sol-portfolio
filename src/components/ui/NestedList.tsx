a74355444a66e4c649b607610855c48e4776f647
sol <soloolol@naver.com>
2025-03-02 16:31:07 +0900

    Feat: [추가] NestedList 컴포넌트 추가 (2/3)

---
+++ src/components/ui/NestedList.tsx
@@ -0,0 +1,20 @@
+export type NestedList = {
+  id: string;
+  label: string;
+  children?: NestedList[];
+};
+
+export const NestedListComponent: React.FC<{ items: NestedList[] }> = ({
+  items,
+}) => {
+  return (
+    <ul className="list-disc list-inside">
+      {items.map((item) => (
+        <li className="pl-3" key={item.id}>
+          {item.label}
+          {item.children && <NestedListComponent items={item.children} />}
+        </li>
+      ))}
+    </ul>
+  );
+};
