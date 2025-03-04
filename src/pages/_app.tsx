e7708522786b9db3741930c852f353002c478677
sol <soloolol@naver.com>
2025-03-05 01:18:06 +0900

    Fix: [수정] _app.tsx QueryClient 생성 방식 변경, getServerSideProps 훅에서 react-query dehydrate 사용하여 데이터 prefetch

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  QueryClient,
+  QueryClientProvider,
+  HydrationBoundary,
+} from '@tanstack/react-query';
 import Layout from '@/components/layout/Layout';
 import Head from 'next/head';
 import { useState } from 'react';
 import ToastContainer from '@/components/toast/ToastContainer';
 
-const queryClient = new QueryClient();
-
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
   const [initDark, setInitDark] = useState<boolean | undefined>(undefined);
 
   const handleDarkModeChange = (changeVal: boolean) => {
@@ -18,19 +21,20 @@ export default function App({ Component, pageProps }: AppProps) {
     } else {
       document.documentElement.classList.remove('dark');
     }
-    console.log('handleDarkModeChange:', changeVal);
     setInitDark(changeVal);
   };
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Head>
-        <title>Sol&apos;s Portfolio</title>
-      </Head>
-      <Layout initDark={initDark} onChangeDarkMode={handleDarkModeChange}>
-        <Component {...pageProps} />
-      </Layout>
-      <ToastContainer />
+      <HydrationBoundary state={pageProps.dehydratedState}>
+        <Head>
+          <title>Sol&apos;s Portfolio</title>
+        </Head>
+        <Layout initDark={initDark} onChangeDarkMode={handleDarkModeChange}>
+          <Component {...pageProps} />
+        </Layout>
+        <ToastContainer />
+      </HydrationBoundary>
     </QueryClientProvider>
   );
 }
